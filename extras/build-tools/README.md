674088397ea4222b8b3960e7d39fe4f843ee1d47
Nicholas de Jong <contact@nicholasdejong.com>
2019-03-16 16:47:25 +0800

    adjustment for new packages repo

--- extras/build-tools/README.md
+++ extras/build-tools/README.md
@@ -1,12 +1,17 @@
-
 # Build Notes
 
-## Create a fresh FreeBSD VirtualBox host
+NB: there does not seem to be any requirement for the FreeBSD host used to build 
+packages with to be the same FreeBSD (or pfSense) version that you are 
+targeting - I note this here because this was not clear to myself when starting
+to build packages.
+
+## Create a fresh FreeBSD host based on VirtualBox
  * Obtain a FreeBSD `VM-IMAGE` based release https://download.freebsd.org/ftp/releases/VM-IMAGES/
  * Recommended image type is VMDK which seems to work well with VirtualBox
  * Decompress the VMDK `xz`, create a new VirtualBox using this HDD image and power-on
  * Login to the instance at the console (root, no password)
  * Perform a package update `pkg update`
+ * Install packages you will need `pkg install rsync`
  * Enable sshd at reboot by adding `sshd_enable="YES"` to the file `/etc/rc.conf`
  * Enable root login via ssh (if this approach suits you)
  * Consider adding your ssh key(s) to login user (again, if this approach suits you)
--- extras/build-tools/build-at-remote.sh
+++ extras/build-tools/build-at-remote.sh
@@ -2,15 +2,16 @@
 
 remote_host=${1}
 remote_user=root
-local_base_path=$(realpath $(dirname $(realpath $0))/../../)
+local_base_path=$(realpath $(dirname ${0})/../../)
+local_packages_path=$(realpath ${local_base_path}/../pfsense_fauxapi_packages)
 
 if [ -z ${remote_host} ]; then
-    echo 'usage: '$0' <host-address>'
+    echo "usage: ${0} <host-address>"
     exit 1
 fi
 
 PORTNAME=pfSense-pkg-FauxAPI
-STAGEDIR=$remote_user@$remote_host:/
+STAGEDIR="${remote_user}@${remote_host}:/"
 
 # push the code to the remote FreeBSD system
 rsync -rv --delete ${local_base_path}/${PORTNAME}/ ${STAGEDIR}/usr/ports/sysutils/${PORTNAME}
@@ -19,8 +20,8 @@ rsync -rv --delete ${local_base_path}/${PORTNAME}/ ${STAGEDIR}/usr/ports/sysutil
 ssh $remote_user@$remote_host "cd /usr/ports/sysutils/${PORTNAME}; make clean; make package"
 
 # pull the .txz packages back
-rsync --ignore-existing $remote_user@$remote_host:/usr/ports/sysutils/${PORTNAME}/work/pkg/*.txz ${local_base_path}/package
+rsync --ignore-existing $remote_user@$remote_host:/usr/ports/sysutils/${PORTNAME}/work/pkg/*.txz ${local_packages_path}
 
 # re-roll the SHA256SUMS
-cd ${local_base_path}/package
-sha256sum pfSense-pkg-FauxAPI-*.txz > SHA256SUMS
+cd ${local_packages_path}
+echo sha256sum pfSense-pkg-FauxAPI-*.txz > SHA256SUMS
